05148d3d9c129c09ae73b9f84f59ecd899a07aa5
Edward Harpham <Edd@mia-bennetts-macbook-pro.local>
2014-07-05 21:32:20 +0100

    Initial Commit

---
+++ ChangingRoomStaff/ProductTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductTableViewCell.h
+//  ChangingRoomStaff
+//
+//  Created by Edward Harpham on 05/07/2014.
+//  Copyright (c) 2014 BlueSkyCoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductTableViewCell : UITableViewCell
+
+@end
